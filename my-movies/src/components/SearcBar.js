558bdc08d32d053a16250e58f6c8d1635b694444
maralpbilisim <72076365+maralpbilisim@users.noreply.github.com>
2022-11-01 17:21:46 +0300

    search item added

--- my-movies/src/components/SearcBar.js
+++ my-movies/src/components/SearcBar.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react'
 
 export default class SearcBar extends Component {
-  state={
-    searchQuery:""
-  }
+  
   handleFormSubmit =(event)=>{
     event.preventDefault();
   }
@@ -13,11 +11,11 @@ export default class SearcBar extends Component {
         <form onSubmit={this.handleFormSubmit}>
           <div className="form-row mb-5">
             <div className="col-12">
-              <input onChange={(event)=>this.setState({searchQuery:event.target.value})} 
+              <input onChange={this.props.searchMovieProp} 
               type="text" 
               className="form-control" 
               placeholder="Search a movie"
-              value={this.state.searchQuery}/>
+             />
             </div>
           </div>
         </form>
