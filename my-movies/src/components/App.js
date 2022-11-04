d5e99be3fd2b24f96d6e9b0862a265b12ebe5fe9
maralpbilisim <72076365+maralpbilisim@users.noreply.github.com>
2022-11-04 15:02:05 +0300

    axios librarry example

--- my-movies/src/components/App.js
+++ my-movies/src/components/App.js
@@ -1,33 +1,64 @@
 import React from "react";
 import SearchBar from './SearcBar'
 import MovieList from './MovieList'
+import axios from 'axios'
 
 class App extends React.Component {
   state = {
     movies: [],
     searchQuery: ""
   }
+  // async componentDidMount(){
+  //   const baseURL="http://localhost:3004/movies"
+  //   const response=await fetch(baseURL)
+  //   console.log(response)
+  //   const data=await response.json();
+  //   console.log(data)
+  //   this.setState({movies:data})
+  // }
   async componentDidMount(){
-    const baseURL="http://localhost:3004/movies"
-    const response=await fetch(baseURL)
-    console.log(response)
-    const data=await response.json();
-    console.log(data)
-    this.setState({movies:data})
+   const response=await axios.get("http://localhost:3004/movies")
+   this.setState({movies:response.data})
   }
   
-  deleteMovie = (movie) => {
-    const newMovieList = this.state.movies.filter(
-      m => m.id !== movie.id
-    )
-    // this.setState({
-    //   movies:newMovieList
-    // })
-    this.setState(state => ({
-      movies: newMovieList
-    }))
+  // deleteMovie = (movie) => {
+  //   const newMovieList = this.state.movies.filter(
+  //     m => m.id !== movie.id
+  //   )
+  //   // this.setState({
+  //   //   movies:newMovieList
+  //   // })
+  //   this.setState(state => ({
+  //     movies: newMovieList
+  //   }))
    
-  }
+// //FETCH API
+//     deleteMovie = async (movie) => {
+//       const baseURL=`http://localhost:3004/movies/${movie.id}`
+//       await fetch(baseURL,{
+//         method:"DELETE"
+//       })
+//       const newMovieList = this.state.movies.filter(
+//         m => m.id !== movie.id
+//       )
+     
+//       this.setState(state => ({
+//         movies: newMovieList
+//       }))    
+//   }
+
+//AXIOS API
+deleteMovie = async (movie) => {
+ axios.delete(`http://localhost:3004/movies/${movie.id}`)
+  
+  const newMovieList = this.state.movies.filter(
+    m => m.id !== movie.id
+  )
+ 
+  this.setState(state => ({
+    movies: newMovieList
+  }))    
+}
   searchMovie=(event)=>{
     console.log(event.target.value)
     this.setState({searchQuery:event.target.value})
